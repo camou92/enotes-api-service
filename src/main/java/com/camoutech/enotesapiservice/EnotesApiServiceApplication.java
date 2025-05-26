16d58c4c68cc05289c7172b7d290c91c86dc44b5
camou92 <cmohamed992@gmail.com>
2025-05-26 01:49:27 +0100

    first commit

---
+++ src/main/java/com/camoutech/enotesapiservice/EnotesApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.camoutech.enotesapiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnotesApiServiceApplication.class, args);
+    }
+
+}
